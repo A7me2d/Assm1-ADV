b3ed77fe798f3ed419634f53e1fef9a544c22b1e
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-07-25 14:41:32 +0300

    Generics

---
+++ Assm1 ADV/Class/Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assm1_ADV.Class
+{
+    internal class Helper<T> 
+    {
+
+        #region Non Generic
+        //public static void SWAP(ref int X, ref int Y)
+        //{
+        //    int Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        //public static void SWAP(ref double X, ref double Y)
+        //{
+        //    double Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        //public static void SWAP(ref Point X, ref Point Y)
+        //{
+        //    Point Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+
+        #endregion
+
+        #region Generic
+        public static void SWAP<T>(ref T X, ref T Y)
+        {
+            T Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+        #endregion
+
+
+    }
+}
--- Assm1 ADV/Program.cs
+++ Assm1 ADV/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Assm1_ADV
+﻿using Assm1_ADV.Class;
+using System.Drawing;
+
+namespace Assm1_ADV
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Generics [SWAP EX]
+
+            //int A = 10, B = 30;
+            //Console.WriteLine($" A={A}");
+            //Console.WriteLine($" B={B}");
+
+            //Helper<int>.SWAP(ref A, ref B);
+
+            //Console.WriteLine("After Swapping");
+            //Console.WriteLine($" A={A}");
+            //Console.WriteLine($" B={B}");
+
+
+            //double A = 2.2, B = 3.3;
+            //Console.WriteLine($" A={A}");
+            //Console.WriteLine($" B={B}");
+
+            //Helper<double>.SWAP(ref A, ref B);
+
+            //Console.WriteLine("After Swapping");
+            //Console.WriteLine($" A={A}");
+            //Console.WriteLine($" B={B}");
+
+            //Point P01 = new Point(1, 2);
+            //Point P02 = new Point(10, 20);
+
+            //Console.WriteLine(P01);
+            //Console.WriteLine(P02);
+
+            //Helper<Point>.SWAP(ref P01, ref P02);
+
+            //Console.WriteLine("After Swapping");
+            //Console.WriteLine(P01);
+            //Console.WriteLine(P02);
+
+            #endregion
+
+
+
+
+
+
         }
     }
 }
